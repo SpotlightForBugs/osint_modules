8bafac0f0cdf533bf6822ee1b7c8981d9d87f380
Johannes Häusler <73603712+SpotlightForBugs@users.noreply.github.com>
2022-06-01 16:21:27 +0200

    added check for input to be (reachable url)

--- identify_input.py
+++ identify_input.py
@@ -2,6 +2,51 @@
 
 ##a module to get to know what kind of input is given##
 
-import sys
-input_raw = sys.argv
-input = input_raw[0]
+import argparse
+import urllib.request
+import tldextract
+
+
+parser=argparse.ArgumentParser()
+parser.add_argument(
+dest='input'
+)
+args = parser.parse_args()
+
+input_raw = args.input
+input = input_raw
+
+
+
+
+
+
+def is_url(): 
+  
+
+    url = input
+    parsed = tldextract.extract(url)
+    domain_without_proto = parsed.domain + '.' + parsed.suffix
+    domain = 'http://' + domain_without_proto
+    try:
+        http_status = urllib.request.urlopen(domain).getcode()
+     
+        if http_status == 200:
+            return True
+    except:
+            return False
+
+
+input_is_url = is_url()
+
+
+
+
+
+
+
+
+
+
+
+
